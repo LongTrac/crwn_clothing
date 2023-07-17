a0109457157e8bb02ccb5819bb013da310cbef79
LongTrac <lokesp123@gmail.com>
2023-07-17 17:56:46 -0500

    0.9.1: Moving on to the shop page, added a simple file representing the product

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { Fragment } from 'react';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 
-const Shop = () => {
-  return (<h1>I am the beibu</h1>);
-}
 const App = () => {
   return (
     <Fragment>
