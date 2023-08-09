6d4f39f5097556e43ebdbd38d278c29fbc7c6f2d
LongTrac <lokesp123@gmail.com>
2023-08-08 19:13:49 -0500

    1.2.0: Convert User Context to use reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { createUserDocFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 
 //Context consist of 2 different parts
@@ -8,11 +8,50 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+
+export const USER_ACTION_TYPE = {
+    SET_CURRENT_USER : 'SET_CURRENT_USER'
+}
+//REDUCER:-------------------------------------------------------------------------------------------------------------
+const userReducer = (state, action) => {
+    const {type, payload} = action;         //meaning: based of this type I want to return/update this obj within payload
+
+    console.log('dispatch');
+    console.log(action);
+    switch(type){
+        case USER_ACTION_TYPE.SET_CURRENT_USER:
+            return (
+                //return an obj that spread thru any of the unaffected attr and upthat the one needed
+                {   ...state,
+                    currentUser: payload
+                }
+            );
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null,
+}
+//-----------------------------------------------------------------------------------------------------------------------
+
+
 //the provider: is the component that will wrap around any  other component that need access to the context inside 
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser] = useState(null);
-    
+    //const [currentUser, setCurrentUser] = useState(null);
+    //USEREDUCER HOOK :  take in a reducer and init state, spit out a state and a dispatcher
+    const [state, dispatch] = useReducer(userReducer,INITIAL_STATE);     //take in a reducer and init state
+    const {currentUser} = state
+
+    console.log(currentUser);
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPE.SET_CURRENT_USER, payload:user })
+    }
+
+
     //run this once when the component mounted, 
     //useeffect will also run whatever you return from the callback when it unmounted
     useEffect(() => {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,7 +17,6 @@ const Navigation = () => {
 
     const { isCartOpen } = useContext(CartContext);
 
-    console.log(currentUser)
     return (
         <Fragment>
             <NavContainer>
